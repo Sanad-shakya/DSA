22bcf373034914228fbcc3358943e726e4ce3d11
Sanad Shakya <shakyasanad4@gmail.com>
2026-09-23 09:41:22 +0530

    Time: 60 ms (5.39%) | Memory: 146.7 MB (6.45%) - LeetSync

---
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -0,0 +1,38 @@
+class Solution {
+
+
+int [][] dp = new int [1005][1005];
+    int fun(int i, int j, int []t, int[] v) {
+
+        // Boundary condition FIRST
+        if (i >= t.length || j >= v.length ) {
+            return 0;
+        }
+        if( dp[i][j] != -1){
+           
+           return dp[i][j];
+
+        }
+
+        if (  t[i] == v[j]) {
+            dp[i][j] = 1 + fun(i + 1, j + 1, t, v);
+return dp[i][j];
+        }
+
+        int c1 = fun(i + 1, j, t, v);
+        int c2 = fun(i, j + 1, t, v);
+
+        return dp[i][j] = Math.max(c1, c2);
+    }
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        
+for (int i = 0; i < dp.length; i++) {
+    for (int j = 0; j < dp[i].length; j++) {
+        dp[i][j] = -1;
+    }
+}
+   return fun(0, 0, nums1, nums2);
+
+
+    }
+}
