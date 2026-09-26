a72fe7f9263d62aee6f8ab2a7160ae2601bd9e3c
Sanad Shakya <shakyasanad4@gmail.com>
2026-09-26 09:28:10 +0530

    Time: 14 ms (38.19%) | Memory: 47 MB (51.07%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -2,39 +2,61 @@ import java.util.*;
 
 class Solution {
 
-    int[][] dp = new int[1005][1005];
+    int[] dp = new int[1005];
 
-    public int fun(int[][] pair, int i, int p) {
+    public int fun(int[][] pair, int i  ) {
 
         if (i == pair.length) {
             return 0;
         }
 
-        if (dp[i][p + 1] != -1) {
-            return dp[i][p + 1];
+        if (dp[i]!= -1) {
+            return dp[i];
         }
+
+int id = pair.length;
+
+int l = i + 1;
+int h = pair.length-1;
+
+while( l  <=h ){
+
+    int mid = (l + h )/2;
+
+if (pair[mid][0] > pair[i][1]) {
+    id = mid;
+    h = mid - 1;
+}
+
+    else 
+    l = mid + 1;
+}
+
+
+
+
 
         // Not take
-        int nottake = fun(pair, i + 1, p);
+ int take = 1 + fun(pair, id);
+int nottake = fun(pair, i + 1);
 
         // Take
-        int take = 0;
+        // int take = 0;
 
-        if (p == -1 || pair[i][0] > pair[p][1]) {
-            take = 1 + fun(pair, i + 1, i);
-        }
+        // if (p == -1 || pair[i][0] > pair[p][1]) {
+        //     take = 1 + fun(pair, i + 1, i);
+        // }
 
-        return dp[i][p + 1] = Math.max(take, nottake);
+        return dp[i] = Math.max(take, nottake);
     }
 
     public int findLongestChain(int[][] pairs) {
 
-        for (int i = 0; i < 1005; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+     
+            Arrays.fill(dp, -1);
+        
+  Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
 
-        Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
-
-        return fun(pairs, 0, -1);
+        return fun(pairs, 0 );
     }
 }
